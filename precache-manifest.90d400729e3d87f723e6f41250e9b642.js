6d60a28b9fe2bab53235932f937ceebd9492a3af
stantheman12-code <stanleylee12@gmail.com>
2022-12-16 15:57:34 +0800

    Updates

---
+++ precache-manifest.90d400729e3d87f723e6f41250e9b642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8788d49972c85acf4c78",
+    "url": "/stantheman12-code/muirheadfc/static/css/main.33360340.chunk.css"
+  },
+  {
+    "revision": "8788d49972c85acf4c78",
+    "url": "/stantheman12-code/muirheadfc/static/js/main.8788d499.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/stantheman12-code/muirheadfc/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "801639ea8ea246d791d1",
+    "url": "/stantheman12-code/muirheadfc/static/js/2.801639ea.chunk.js"
+  },
+  {
+    "revision": "4b5e7a3f837312746b78",
+    "url": "/stantheman12-code/muirheadfc/static/js/runtime~main.4b5e7a3f.js"
+  },
+  {
+    "revision": "df3908e03c1704a9854abe0913668312",
+    "url": "/stantheman12-code/muirheadfc/index.html"
+  }
+];
